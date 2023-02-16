cb59199e0e9c687288291aa56283cb504f32d076
Camden Conrad <90994136+latteBruh@users.noreply.github.com>
2023-02-16 18:35:06 -0500

    Finished

---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿namespace FileIO;
+using System.IO;
+
+public class Io
+{
+    public static void Main()
+    {
+
+
+        String path = "Numbers.txt";
+        if (!File.Exists(path))
+        {
+            return;
+        }
+        
+        StreamWriter evenWriter = new StreamWriter("Even.txt");
+        StreamWriter oddWriter = new StreamWriter("Odd.txt");
+        
+        using (StreamReader stream = File.OpenText(path))
+        {
+            string? next;
+            while ((next = stream.ReadLine()) != null)
+            {
+                if (int.Parse(next) % 2 == 0)
+                {
+                    evenWriter.WriteLine(next);
+                }
+                else
+                {
+                    oddWriter.WriteLine(next);
+                }
+            }
+        }
+        evenWriter.Close();
+        oddWriter.Close();
+
+    }
+    
+}
